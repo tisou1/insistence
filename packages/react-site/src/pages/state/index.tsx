61a782e0e62bbbd7d538cebefc2f8c5dbc3f1261
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-06-01 13:56:37 +0800

    feat: add state

--- packages/react-site/src/pages/state/index.tsx
+++ packages/react-site/src/pages/state/index.tsx
@@ -1,5 +1,43 @@
+import React from 'react'
 export default function Index() {
   return (
-    <div>Index</div>
+    <div>
+      <Tick />
+    </div>
   )
+}
+
+interface State {
+  date: Date
+}
+interface Props {
+  [args: string]: unknown
+}
+class Tick extends React.Component<Props, State>{
+  constructor(props: Props){
+    super(props)
+    this.state = {date: new Date()}
+  }
+  timerID: any
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+  componentDidUnMount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+  render(){
+    return (
+      <div>
+        <h1>hello, world</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
 }
