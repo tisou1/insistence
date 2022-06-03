f4fd1bd8617b08f0dbc5d14080fb2e1540db053b
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-06-03 09:56:46 +0800

    chore(basic): async

--- packages/basic/main.ts
+++ packages/basic/main.ts
@@ -1,2 +1,22 @@
 import './index.css'
-console.log('main')
+
+async function  getName(params: string) {
+    const n1 = await 'siry'
+    const n2 = await 'tisou1'
+
+    return n2
+}
+
+getName('cc').then(res => console.log(res))
+
+
+/**
+ * 
+ * await 后面是Promise对象时,返回该对象的结果; 如果不是Promise对象,直接返回其值.
+ */
+async function  getC() {
+  return await 333
+  //等同于 return 333
+}
+
+getC().then(res => console.log(res))
