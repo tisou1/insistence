7e797309343371880ec04343cc357a696ab45eda
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-06-04 20:42:44 +0800

    chore: hoc

--- packages/react-site/src/pages/advanced/hoc.tsx
+++ packages/react-site/src/pages/advanced/hoc.tsx
@@ -3,11 +3,19 @@ import React, { Component } from 'react'
 export default class Hoc extends Component {
   render() {
     return (
-      <div>Hoc</div>
+      <div>Hoc
+        <Test message1='&lt;3' message2={'&lt;3'}/>
+      </div>
     )
   }
 }
 
 //hoc
 
+function Test(props: any) {
+  // message2不会进行转义的
+  return (
+    <div>{props.message1} |  {props.message2}</div>
+  )
+}
 
