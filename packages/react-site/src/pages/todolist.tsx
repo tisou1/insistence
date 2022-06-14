e334c62b1667238d7309ce9e84c66407325c2a5f
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-06-14 20:07:08 +0800

    chore(react-l): todolist

--- packages/react-site/src/pages/todolist.tsx
+++ packages/react-site/src/pages/todolist.tsx
@@ -26,33 +26,38 @@ function InputValue(props: { onChange: (value: string) => void }) {
   }
   return (
     <div className='flex gap-4 my-3 justify-between'>
-      <input type='text' value={value} onChange={onChange} className="grow border-solid border-gray-400/50 rounded-12px h-42px outline-none focus:border-orange-500/30 text-18px text-gray-300"/>
+      <input type='text' value={value} onChange={onChange} className="px-3 grow border-solid border-gray-400/50 rounded-12px h-42px outline-none focus:border-orange-500/30 text-18px text-gray-300"/>
       <button onClick={onClick} className="btn px-8 rounded-20px">增加</button>
     </div>
   )
 }
 
-function Item(props: { item: Item, onDelete: (idx: number) => void,idx: number}) {
-  // const onDelete = useContext()
-  const onDelete = () => {
-    console.log('delete')
-    props.onDelete(props.idx)
-  }
+// function Item(props: { item: Item, onDelete: (idx: number) => void,idx: number}) {
+//   // const onDelete = useContext()
+//   const onDelete = () => {
+//     console.log('delete')
+//     props.onDelete(props.idx)
+//   }
 
-  return (
-    <div className='flex pa-3 hover:bg-gray-200/30 justify-between'>
-      <span>{props.item?.name}</span>
-      <button onClick={() => onDelete()} className="i-carbon-close text-red-700/30 hover:text-red-700/90 cursor-pointer w-24px h-24px" />
-    </div>
-  )
-}
+//   return (
+//     <div className='flex pa-3 hover:bg-gray-200/30 justify-between'>
+//       <span>{props.item?.name}</span>
+//       <button onClick={() => onDelete()} className="i-carbon-close text-red-700/30 hover:text-red-700/90 cursor-pointer w-24px h-24px" />
+//     </div>
+//   )
+// }
 
 function List(props: { list: Item[], onDelete: (idx: number) => void}) {
+
   return (
     <div className='flex flex-col overflow-hidden bg-gray-300/10 rounded-xl'>
       {
         props.list.map((item: Item, idx: number) => (
-          <Item item={item} key={item.id} idx={idx} onDelete={props.onDelete}/>
+          <div key={item.id} className='flex pa-3 hover:bg-gray-200/30 justify-between'>
+            <span>{item?.name}</span>
+            <button onClick={() => props.onDelete(idx)} className="i-carbon-close text-red-700/30 hover:text-red-700/90 cursor-pointer w-24px h-24px" />
+          </div>
+          // <Item item={item} key={item.id} idx={idx} onDelete={props.onDelete}/>
         ))
       }
       {
---
+++ packages/vue-l/src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+
+<div>todolist</div>
+</template>
+
+<style scoped>
+
+</style>
