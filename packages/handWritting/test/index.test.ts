ead48080991230f85a2e3cad7da7c773270077fd
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-08-21 10:14:26 +0800

    chore: add index

--- packages/handWritting/test/index.test.ts
+++ packages/handWritting/test/index.test.ts
@@ -1,33 +1,25 @@
 import { afterEach } from "vitest";
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import { debounce } from '../debounce'
-import { unique, unique2 } from '../quchong'
-import { reduce } from '../reduce'
+import { debounce, unique, unique2, reduce } from '../index'
 
 const mock = vi.fn(() => console.log('executed'))
 
 describe('测试手写函数', () => {
   beforeEach(() => {
-    //告诉vitest,我们使用模拟事件
+    //告诉vitest,我们使用模拟时间
     vi.useFakeTimers()
   })
   afterEach(() => {
     vi.restoreAllMocks()
   })
   it('防抖函数', () => {
-    setInterval(debounce(mock), 100)
-    // vi.runAllTimers()
-    // expect(mock).toHaveBeenCalledTimes(1)
-    vi.advanceTimersToNextTimer()
-    expect(mock).toHaveBeenCalledTimes(1)
+    const debounced = debounce(mock, 1000)
 
-    vi.advanceTimersToNextTimer()
-    expect(mock).toHaveBeenCalledTimes(1)
+    debounced()
+    debounced()
+    debounced()
+    vi.runAllTimers()
 
-    vi.advanceTimersToNextTimer()
-    expect(mock).toHaveBeenCalledTimes(1)
-
-    vi.advanceTimersToNextTimer()
     expect(mock).toHaveBeenCalledTimes(1)
   })
 
@@ -38,9 +30,12 @@ describe('测试手写函数', () => {
     expect(unique2(arr)).toEqual(expectArr)
   })
 
-
+  //reduce
   it('reduce', () => {
     const arr = [1, 2, 3, 4, 5]
     expect(reduce(arr, (a: number, b: number) => a + b)).toBe(15)
   })
+
+
+  //
 })
